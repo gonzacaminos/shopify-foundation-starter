29e97a618e48105a4488c1cd0755d7c5bb9bdec9
gonzacaminos <5827300+gonzalobonini@users.noreply.github.com>
2022-12-01 12:31:02 -0300

    Added swiper for collections

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@
 import './scss/main.scss'
 import Alpine from 'alpinejs'
 import GoCart from '@dango-digital/gocart';
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+import {Ajaxinate} from 'ajaxinate';
 
 window.Alpine = Alpine
 
@@ -17,3 +19,57 @@ const goCartOptions = {
    }
    
 const goCart = new GoCart(goCartOptions);
+const swiper_options = (name) => ({
+  modules: [Navigation, Pagination],
+  pagination: {
+      el: `.swiper-pagination.pagination-${name}`,
+      clickable: true
+    },
+  navigation: {
+      nextEl: `.swiper-button-next.button-${name}`,
+      prevEl: `.swiper-button-prev.button-${name}`,
+      lockClass: 'lock',
+      navigationDisabledClass: 'dis'
+  }
+})
+
+
+const carousels = document.querySelectorAll('.swiper');
+
+carousels.forEach(function(carousel) {
+  var swiper = new Swiper(carousel, swiper_options(carousel.id));
+});
+
+const swiper_scroll = new Swiper(".swiper-scroll", {
+  modules: [Navigation, Pagination, Scrollbar],
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
+  centeredSlides: false,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+    draggable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next.scroll",
+    prevEl: ".swiper-button-prev.scroll",
+  }
+});
